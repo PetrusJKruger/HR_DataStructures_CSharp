7729953947051123649b87386b8949097c1ad992
PetrusJKruger <61282927+PetrusJKruger@users.noreply.github.com>
2020-03-25 06:38:17 +0200

    Update HR_CompareTriplets.cs

--- HR_CompareTriplets.cs
+++ HR_CompareTriplets.cs
@@ -8,24 +8,7 @@
       
    ******************************* */
 
-
-
-
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
-class Solution {
+ /* add code where appropriate */
 
     // Complete the compareTriplets function below.
     static List<int> compareTriplets(List<int> a, List<int> b) {
@@ -51,19 +34,3 @@ class Solution {
         return scores;
 
     }
-
-    static void Main(string[] args) {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
-
-        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
-
-        List<int> result = compareTriplets(a, b);
-
-        textWriter.WriteLine(String.Join(" ", result));
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
